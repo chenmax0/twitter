df308f74c0bfc18eab76d7de45583904fe2a16dc
max <dev.max.chen@gmail.com>
2026-05-11 22:36:26 +0200

    chore(authentication): create users table

---
+++ server/migrations/20260511145249_create_users_table.up.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS "users" (
+  id UUID PRIMARY KEY,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  pseudo VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE INDEX idx_user_email ON users(email);
+CREATE INDEX idx_user_pseudo ON users(pseudo);
